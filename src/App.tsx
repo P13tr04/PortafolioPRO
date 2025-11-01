840abc7275a0144c0f11a958c864b78ca94ebeee
Pietro Sánchez <PietroSTusa17@Gmail.com>
2025-10-31 23:04:37 -0400

    Add Gracias route and update layout styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { Routes, Route, Link } from "react-router-dom";
 import Inicio from "./Inicio";
 //@ts-ignore
 import Contacto from "./Contacto.jsx";
+//@ts-ignore
+import Gracias from "./Gracias";
 
 export default function App() {
   return (
-    <div className="w-full max-w-3xl">
-      <nav className="flex justify-center gap-6 mb-4">
+    <div className="w-full max-w-3xl mx-auto p-4">
+      <nav className="flex justify-center gap-6 mb-8">
         <Link
           to="/"
           className="text-lg font-semibold text-slate-400 hover:text-blue-400 transition-colors duration-300"
@@ -24,6 +26,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Inicio />} />
         <Route path="/contacto" element={<Contacto />} />
+        <Route path="/gracias" element={<Gracias />} />
       </Routes>
     </div>
   );
